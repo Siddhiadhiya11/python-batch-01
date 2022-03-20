8635e07ed083f2297dcef96f4d6d233111b4a6f9
siddhi adhiya <siddhiadhiya@gmail.com>
2022-03-20 09:01:17 +0530

    commit

--- oop/super.py
+++ oop/super.py
@@ -1,41 +1,41 @@
-class Person:
-    country = 'India'
-    
-    def __init__(self):
-        print('Person Initialized')
-
-    def singNationalAnthem(self):
-        print('Person is singing national anthem')
-
-class Employee(Person):
-    company = 'Dell'
-
-    def __init__(self):
-        super().__init__()
-        print('Employee Initialised')
-        
-    def singNationalAnthem(self):
-        super().singNationalAnthem()
-        print('Employee is singing national anthem')
+# Python program to demonstrate
+# super function
 
 
-class Programmer(Employee):
+class Animals:
+	
+	# Initializing constructor
+	def __init__(self):
+		self.legs = 4
+		self.domestic = True
+		self.tail = True
+		self.mammals = True
+	
+	def isMammal(self):
+		if self.mammals:
+			print("It is a mammal.")
+	
+	def isDomestic(self):
+		if self.domestic:
+			print("It is a domestic animal.")
+	
+class Dogs(Animals):
+	def __init__(self):
+		super().__init__()
 
-    company = "Twitter"
+	def isMammal(self):
+		super().isMammal()
 
-    def __init__(self):
-        super().__init__()
-        # print('Programmer initialized')
+class Horses(Animals):
+	def __init__(self):
+		super().__init__()
 
-    def getSalary(self):
-        print('there is no salary')
-    
-    def singNationalAnthem(self):
-        super().singNationalAnthem()
-        print('Programmer is singing national anthem')
+	def hasTailandLegs(self):
+		if self.tail and self.legs == 4:
+			print("Has legs and tail")
 
-
-p = Programmer()
-# print(p.company)
-# print(p.country)
-p.singNationalAnthem()
+# Driver code
+Tom = Dogs()
+Tom.isMammal()
+Bruno = Horses()
+Bruno.hasTailandLegs()
